2d24c62a3f5e8f679f75628d2a33ae5cab312ff4
yana-157 <yanaodanzig@gmail.com>
2024-11-24 22:23:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+install cmu_graphics
+from cmu_graphics import *
+def onAppStart(app):
+	app.width = 800
+	app.height = 600
+def redrawAll(app):
+  drawLabel("Garden Tycoon", app.width / 2, app.height / 2, size=30, fill="blue")
